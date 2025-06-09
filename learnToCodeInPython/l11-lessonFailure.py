82f35fe602f5d2081b7154e02aab07518be28c48
P_8477 <felipe.bomfim@camara.leg.br>
2025-06-09 11:07:22 -0300

    Commit inicial

---
+++ learnToCodeInPython/l11-lessonFailure.py
@@ -0,0 +1,4 @@
+# print("Starting up game server...')
+# prnt("local game server is listening on port 8080")
+print("Starting up game server...")
+print("local game server is listening on port 8080")
---
+++ learnToCodeInPython/l13-shopResponse.py
@@ -0,0 +1,2 @@
+print("The Innkeeper: Ah! Great choices...")
+print("The Innkeeper: Is there anything else I can help you with?")
