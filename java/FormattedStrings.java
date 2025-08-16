ba78df1b58dca4db312c95d2b56a51daf49c171c
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-08-16 11:21:50 -0300

    Completed formatted strings lesson

---
+++ java/FormattedStrings.java
@@ -0,0 +1,9 @@
+public class FormattedStrings {
+    public static void main(String[] args) {
+        String name = "Yarl";
+        int age = 37;
+        String race = "dwarf";
+
+        System.out.printf("%s is %s who is %d years old.\n", name, race, age);
+    }
+}
