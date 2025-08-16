7be98bfa2d62d0e47d67cb984cc5e60dd9f80858
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-08-16 11:29:49 -0300

    Completed the math with strings lesson

---
+++ java/ConcatenateStrings.java
@@ -0,0 +1,12 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String sentenceStart = "You have ";
+        String sentenceEnd = " health";
+
+        String player1Health = "1200";
+        String player2Health = "1100";
+
+        System.out.println(sentenceStart+player1Health+sentenceEnd);
+        System.out.println(sentenceStart+player2Health+sentenceEnd);
+    }
+}
